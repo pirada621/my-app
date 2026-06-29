d096d8e62a09ed5661fe378d077f157ccd59b79f
pirada621 <pirada621@gmail.com>
2026-06-29 17:03:00 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/',function () {
+    return view("welcome");
+});
+
+Route::get('/about',function () {
+    return view("about");
+});
+
+Route::get('/blog',function () {
+    return view("blog");
+});
+Route::get('/workshop',function () {
+    return view("workshop");
 });
