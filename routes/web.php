4e1cf726e47c56b40e99bf65f8efcb604876f617
pirada621 <pirada621@gmail.com>
2026-07-06 16:05:20 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',function () {
-    return view("welcome");
-});
+    return view("index");
+})->name('index');
 
 Route::get('/about',function () {
     return view("about");
@@ -13,6 +13,34 @@ Route::get('/about',function () {
 Route::get('/blog',function () {
     return view("blog");
 });
+Route::get('/abouts',function () {
+    $name = "Pirada";
+    $date = "6 กรกฏาคม 2569";
+    return view("abouts",compact("name","date"));
+})->name('abouts');
+
+Route::get('/blogs',function () {
+    $blogs = [
+        ['title'=> 'บทความที่ 1','content'=>'เนื้อหาบทความที่ 1',
+         'content'=>'เนื้อหาบทความที่ 1',
+         'status'=>true
+        ],
+        [
+         'title'=> 'บทความที่ 2',
+         'content'=>'เนื้อหาบทความที่ 2',
+         'status'=>true 
+        ],
+        [
+        'title'=> 'บทความที่ 3',
+         'content'=>'เนื้อหาบทความที่ 3',
+         'status'=>false     
+        ]
+    ];
+    return view("blogs",compact("blogs"));
+})->name('blogs'); 
+
 Route::get('/workshop',function () {
     return view("workshop");
 });
+
+
